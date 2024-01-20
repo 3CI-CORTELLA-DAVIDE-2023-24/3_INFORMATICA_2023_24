b4e8c2c950894193e1f371e1717a75668b676e3f
3CI_CORTELLA_DAVIDE_2023_24 <148719273+3CI-CORTELLA-DAVIDE-2023-24@users.noreply.github.com>
2024-01-20 19:14:57 +0100

    Update and rename Es_02.java to Prodotto.java

---
+++ BIN/Compiti vacanze/Lab_10_11_cortella/Prodotto.java
@@ -0,0 +1,36 @@
+package Lab_10_11_cortella;
+
+
+/**
+ * Calcolare e visualizzare  il prodotto di n valori interi
+ * @author Cortella Davide 
+ * @version 1:0
+ */
+import javax.swing.JOptionPane;
+public classProdotto{
+    public static void main(String args[]){
+        int val1,val2,i,prod;
+        n=2;
+        prod=0;
+        do{
+            val1=Integer.parseInt(JOptionPane.showInputDialog("dimmi quante volte vuoi sommare b"));     
+        }while(a<1);
+        for(i=0;i<val1;i++){
+            do{
+                val2=Integer.parseInt(JOptionPane.showInputDialog("inserisci il fattore di moltiplicazione"));
+
+            }while(val2<1);
+            prod=prod+val2;
+        }
+        System.out.println ("il prodotto è:"+prod);
+    }
+}
+
+            
+        
+        
+        
+        
+    
+
+    
