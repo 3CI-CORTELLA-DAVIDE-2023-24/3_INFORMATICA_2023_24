29b3a41445e2724ce6f2865164bdd8d4f202884f
3CI_CORTELLA_DAVIDE_2023_24 <148719273+3CI-CORTELLA-DAVIDE-2023-24@users.noreply.github.com>
2024-01-21 15:46:37 +0100

    Update and rename Es_4B.java to TabellinaDel21.java

--- BIN/Lab_1_12_cortella/Lab_1_12_cortella/TabellinaDel21.java
+++ BIN/Lab_1_12_cortella/Lab_1_12_cortella/TabellinaDel21.java
@@ -1,13 +1,12 @@
 
 /**
- * Aggiungi qui una descrizione della classe Es_4B
- * 
+ * Stampare la tabellina del 21 
  * @author (Davide Cortella) 
  * @version (1:0)
  */
 import java.util.*;
 import java.io.*;
-public class Es_4B{
+public class TabellinaDel21{
     public static void main(String[]args){
         int n = 21 , i = 0;
         Scanner inp = new Scanner (System.in);
