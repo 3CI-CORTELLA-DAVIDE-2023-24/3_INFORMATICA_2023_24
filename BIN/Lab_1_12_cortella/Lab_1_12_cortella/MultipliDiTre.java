5d9514eae750d11de5aca0d4d2769102cdbcbd7f
3CI_CORTELLA_DAVIDE_2023_24 <148719273+3CI-CORTELLA-DAVIDE-2023-24@users.noreply.github.com>
2024-01-21 15:44:05 +0100

    Update and rename Es_4A.java to MultipliDiTre.java

--- BIN/Lab_1_12_cortella/Lab_1_12_cortella/MultipliDiTre.java
+++ BIN/Lab_1_12_cortella/Lab_1_12_cortella/MultipliDiTre.java
@@ -1,12 +1,11 @@
 
 /**
- * Aggiungi qui una descrizione della classe Es_4A
- * 
+ * Stampare i multipli di 3 tra 1 e n inserito dall'utente 
  * @author (Davide Cortella) 
  * @version 1:0
  */
 import javax.swing.*;
-public class Es_4A
+public class MultipliDITre
 {
     public static void main(String [] args){
         int n;
