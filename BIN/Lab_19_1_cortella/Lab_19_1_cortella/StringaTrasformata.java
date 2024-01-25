93949402518f73858a9e680ef30b15bf8a8abc03
3CI_CORTELLA_DAVIDE_2023_24 <148719273+3CI-CORTELLA-DAVIDE-2023-24@users.noreply.github.com>
2024-01-25 22:19:19 +0100

    Update and rename Es_07.java to StringaTrasformata.java

--- BIN/Lab_19_1_cortella/Lab_19_1_cortella/StringaTrasformata.java
+++ BIN/Lab_19_1_cortella/Lab_19_1_cortella/StringaTrasformata.java
@@ -2,12 +2,11 @@
 /**
  * Trasformare una stringa in modo che abbia l’iniziale maiuscola e tutte le altre lettere
 minuscole.
- * 
  * @author Davide Cortella
  * @version 1.0
  */
 import javax.swing.*;
-public class Es_07{
+public class StringaTrasformata{
     public static void main(String[]args){
         String parola=JOptionPane.showInputDialog("dimmi la parola");
         char Primalettera= parola.charAt(0);
