09ac221b3c2066a1b370846dbe016dd1072143d5
3CI_CORTELLA_DAVIDE_2023_24 <148719273+3CI-CORTELLA-DAVIDE-2023-24@users.noreply.github.com>
2024-01-25 22:16:31 +0100

    Update and rename Es_05.java to EmailMarconiAbbreviata.java

--- BIN/Lab_19_1_cortella/Lab_19_1_cortella/EmailMarconiAbbreviata.java
+++ BIN/Lab_19_1_cortella/Lab_19_1_cortella/EmailMarconiAbbreviata.java
@@ -2,13 +2,12 @@
 /**
  * Presi un nome ed un cognome, creare l’indirizzo email del marconi, formato dalla prima
 lettera del nome e dalle prime quattro del cognome, seguiti da
-
 @studenti.marconiverona.edu.it (mario rossi → mross@ studenti.marconiverona.edu.it )
  * @author Davide Cortella 
  * @version 1.0
  */
 import javax.swing.*;
-public class Es_05{
+public class EmailMarconiAbbreviata{
     public static void main(String []args){
         String Nome=JOptionPane.showInputDialog("Dimmi il nome");
         String Cognome=JOptionPane.showInputDialog("Dimmi il cognome");
