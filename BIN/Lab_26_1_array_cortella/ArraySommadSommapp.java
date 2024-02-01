4b83e68ea2c3b1d1735b3f97f92145b75c665a24
3CI_CORTELLA_DAVIDE_2023_24 <148719273+3CI-CORTELLA-DAVIDE-2023-24@users.noreply.github.com>
2024-02-01 23:55:10 +0100

    Update and rename Es_01.java to ArraySommadSommapp.java

--- BIN/Lab_26_1_array_cortella/ArraySommadSommapp.java
+++ BIN/Lab_26_1_array_cortella/ArraySommadSommapp.java
@@ -12,7 +12,7 @@ import javax.swing.*;
 import java.util.*;
 import java.io.*;
 
-public class Es_01{
+public class ArraySommaSommadSommapp{
     public static void main(String[]args){
         int l;
         int v[];
